5f3abe74a7093b80101fd0f029c98a41f4757c14
Zach Smith <x.coder.zach@gmail.com>
2012-04-13 02:59:41 -0500

    cull chats

--- app/models/chat.js
+++ app/models/chat.js
@@ -3,3 +3,13 @@ var Document = require("LiveDocument/lib/document")
 var Chat = module.exports = Document.define("Chat")
   .key("name", { length: [1, 18] })
   .key("content", { length: [1, 140] })
+  .afterSave(function() {
+    //cull some chats, to keep the number down
+    Chat.find({}, function(chats) {
+      var num = chats.length - 100
+      chats.sortBy("createdAt", true)
+      for(i = 0 ; i < num ; i++) {
+        chats.at(chats.length - (i+1)).remove()
+      }
+    })
+  })
